aead9bf99b58fc125583d24cccb41da972977f61
vijay <gadgetzone@gadgetzones-MacBook-Air-2.local>
2025-07-05 17:31:15 +0530

    myapp

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+}
